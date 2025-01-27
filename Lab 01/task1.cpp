d3fa92631724908687d46b1fdd2e09a7cfe1f9b4
Laiba Ashfaq Adamji <laibaadamji@gmail.com>
2025-01-27 19:24:44 +0500

    Update task1.cpp

--- Lab 01/task1.cpp
+++ Lab 01/task1.cpp
@@ -1,1 +1,25 @@
 
+#include<iostream>
+using namespace std;
+int main(){
+    int nums[5], i;
+    for (i = 0; i < 5; i++) {
+        cout << "Enter number: " << i + 1 << ": ";
+        cin >> nums[i];
+    }
+    int *max = 0, *secLarge = 0;
+    for (i = 0; i < 5; i++) {
+        if (!max || nums[i] > *max) {
+            secLarge = max;
+            max = &nums[i];
+        } else if ((!secLarge || nums[i] > *secLarge) && nums[i] != *max) {
+            secLarge = &nums[i];
+        }
+    }
+    if (secLarge) {
+        cout << "the second largest number in the array is: " << *secLarge << endl;
+    } else {
+        cout << "No second largest number found lol loser";
+    }
+    return 0;
+}
